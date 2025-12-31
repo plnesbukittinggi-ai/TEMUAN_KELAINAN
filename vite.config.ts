bd00c056e271c122c2fbd916b15d3d4ee1a4b215
plnesbukittinggi-ai <plnes.bukittinggi@gmail.com>
2025-12-31 14:05:57 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+    // Menghindari error "process is not defined" di browser
+    'process.env': {
+      API_KEY: JSON.stringify(process.env.API_KEY || '')
+    }
   },
   server: {
     port: 3000
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: false
   }
 });
