394c63499094e07ab57575c4c9c5aca264bd9b60
plnesbukittinggi-ai <plnes.bukittinggi@gmail.com>
2026-01-21 16:08:39 +0700

    Update types.ts

--- types.ts
+++ types.ts
@@ -48,7 +48,7 @@ export interface TemuanData {
   geotag?: string; // Koordinat GPS (Latitude, Longitude)
   fotoTemuan: string; 
   keterangan: string;
-  status: 'BELUM EKSEKUSI' | 'SUDAH EKSEKUSI' | 'BUTUH PADAM';
+  status: 'BELUM EKSEKUSI' | 'SUDAH EKSEKUSI' | 'BUTUH PADAM' | 'BUTUH IZIN TEBANG';
   tanggalEksekusi?: string;
   fotoEksekusi?: string;
   timEksekusi?: string;
