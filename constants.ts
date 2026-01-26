23b010a1c9bacc90a5b78110ba732aebba7e4f9f
plnesbukittinggi-ai <plnes.bukittinggi@gmail.com>
2026-01-26 21:13:11 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,13 +3,13 @@ import { Inspector, ULP, Feeder, Keterangan, Pekerjaan } from './types';
 
 export const SPREADSHEET_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxbx5jjaOHOPtUBatlAjTYaGnDXJKUB96p8fsU9-myfN7N0qtmDDQWzOeZe6Jj5uPsG/exec';
 
-export const APP_VERSION = '1.0.6';
+export const APP_VERSION = '1.0.7';
 
 export const INITIAL_PEKERJAAN: Pekerjaan[] = [
   { id: 'PEK01', name: 'JTM Tier 1' },
-  { id: 'PEK02', name: 'JTM Tier 1 - Tier 2' },
+  { id: 'PEK02', name: 'JTM Tier 1 & 2' },
   { id: 'PEK03', name: 'GARDU Tier 1' },
-  { id: 'PEK04', name: 'GARDU Tier 1 - Tier 2' }
+  { id: 'PEK04', name: 'GARDU Tier 1 & 2' }
 ];
 
 export const INITIAL_INSPECTORS: Inspector[] = [
