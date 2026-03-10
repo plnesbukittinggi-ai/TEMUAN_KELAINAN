f1b2097f776095039b38b6660ab238be2df6cdca
plnesbukittinggi-ai <plnes.bukittinggi@gmail.com>
2026-03-10 19:28:54 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ import { Inspector, ULP, Feeder, Keterangan, Pekerjaan } from './types';
 
 export const SPREADSHEET_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbw_8wCUk4EZcTVwxG3oryedtEEOc24glOKgpug5zPY3S3E7QuDXuzohnMpp_W_jajIN/exec';
 
-export const APP_VERSION = '1.0.7';
+export const APP_VERSION = '2.0.0';
 
 export const INITIAL_PEKERJAAN: Pekerjaan[] = [
   { id: 'PEK01', name: 'JTM TIER 1' },
